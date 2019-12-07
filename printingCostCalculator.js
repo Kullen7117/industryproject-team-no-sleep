c304cf06c436ba3c8a6a37987c7de4acd3c97190
Keefe Laporte <keefelaporte2019@gmail.com.com>
2019-12-07 11:17:40 -0500

    added to calc function

--- printingCostCalculator.js
+++ printingCostCalculator.js
@@ -1,33 +1,56 @@
  /* ----------------------------------------------------------------
- *  OPT Industry Project - Team No Sleep
- *  Purpose - This program will calculate the price of printing a 3d object.
- *  objectives Create a web application. 
- *             Allow the user to upload and view a 3D model (.stl-file).
- *             Calculate key values of the model (volume, material cost etc.).
- *             Provide a PDF export option with a comprehensive report of cost calculation.
- *  
- *  author - Madelyn
- *  author - Keefe
- *  author - Kullen - https://github.com/Kullen7117
- *  author - Daniel Guerrero - gitHub - https://github.com/itsdanielguerrero
- * 
- *  -----------------------------------------------------------------*/
+  *  OPT Industry Project - Team No Sleep
+  *  Purpose - This program will calculate the price of printing a 3d object.
+  *  objectives Create a web application. 
+  *             Allow the user to upload and view a 3D model (.stl-file).
+  *             Calculate key values of the model (volume, material cost etc.).
+  *             Provide a PDF export option with a comprehensive report of cost calculation.
+  *  
+  *  author - Madelyn
+  *  author - Keefe
+  *  author - Kullen - https://github.com/Kullen7117
+  *  author - Daniel Guerrero - gitHub - https://github.com/itsdanielguerrero
+  * 
+  *  -----------------------------------------------------------------*/
 
-function calculatePrintingPrice(volume, printTime, resinType ){
-    //not implemented yet
-    let inLiters = volume * 0.0000010 //convert to Liters
+ function calculatePrintingPrice(volume, printTime, resinType) {
+     //not implemented yet
+     let inLiters = volume * 0.0000010 //convert to Liters
 
-    let totalPrice = inLiters * resinPrice
+     let totalPrice = inLiters * resinPrice
 
-    //switch statment
+     //switch statment
+     switch (resinType) {
+         case 'pcLightgrey':
+             return inLiters * 59.90;
+             break;
+         case 'pcTRed':
+             return inLiters * 69.98;
+             break;
+         case 'pc3D':
+             return inLiters * 76.90;
+             break;
+         case 'bcBlue':
+             return inLiters * 138.69;
+             break;
+         case 'bcX5':
+             return inLiters * 142.99;
+             break;
+         case 'zrsgr':
+             return inLiters * 345.99;
+             break;
+         default:
+             return 0;
+             break;
+     }
 
-    return totalPrice
-}
+     return totalPrice
+ }
 
-var vol = 16273
-var time = 0
-var resinPrice = 59.9
+ var vol = 16273
+ var time = 0
+ var resinPrice = 'zrsgr'
 
-var printPrice = calculatePrintingPrice(vol, time, resinPrice)
+ var printPrice = calculatePrintingPrice(vol, time, resinPrice)
 
-console.log('the price is ' + printPrice + 'boom baby!')
+ console.log('the price is ' + printPrice + 'boom baby!')
